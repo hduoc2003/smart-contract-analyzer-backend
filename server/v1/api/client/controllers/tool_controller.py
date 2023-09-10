59078af3d6746507b3ff4680e957833a50c3a32d
Melted <21020658@vnu.edu.vn>
2023-09-10 22:18:37 +0700

    Add handle contract-result functions

--- server/v1/api/client/controllers/tool_controller.py
+++ server/v1/api/client/controllers/tool_controller.py
@@ -1,4 +1,46 @@
-from flask import Flask, jsonify
+import logging
+from flask import Flask, jsonify, request
+from typing import Any, List
+import tempfile
+import os
+from tools.Tool import Tool, ToolName
+from server.v1.api.utils.StatusCode import StatusCode
+from server.v1.api.utils.FlaskLog import *
+import server.v1.config.app_config
+import json
+from bson import ObjectId  # Import ObjectId for generating unique IDs
+from server.v1.api.client.models.tools_collection import *
 
 def handle_tool():
-    return jsonify({"message": "access_tool_route"})
+    user_name = "phu21122003"
+    tools = ["Mythril", "Slither"]
+    if request is None:
+        return jsonify({"message": "Nothing requested"}), StatusCode.BadRequest.value
+    
+    files_data = request.files
+    file_keys = files_data.keys()
+    
+    print(file_keys)
+    file_list = []
+    for file_key in file_keys:
+        file_data = files_data[file_key]
+        file_name = file_data.filename
+        file_content = file_data.read()
+        
+        # Save the file to local storage
+        save_file_to_local_storage(file_name, file_content, user_name)
+        
+        file_list.append(file_name)
+    
+    print("FILELIST: ", file_list)
+
+    FlaskLog.info(f"{file_list} {tools}  {user_name}")
+    result = Tool.run_tools(file_list, tools, user_name)
+    
+    result_dict = str_to_dict(result)
+
+    create_file_doc(result_dict)
+    
+    add_issue_ids(result_dict)
+    return jsonify(result_dict), StatusCode.OK.value
+
--- server/v1/config/app_config.py
+++ server/v1/config/app_config.py
@@ -7,11 +7,11 @@ from server.v1.api.client.routes.auth_route import auth_route
 from flask_cors import CORS
 from server.v1.api.client.routes.tool_route import tool_route
 from server.v1.api.admin.routes.user.user import user_route
-
+import tools.Tool
 APP_CONFIG: dict[str, Any] = {
     "ALLOWED_ORIGINS": ["http://localhost:3000"]
 }
-    
+storage_path: str = tools.Tool.Tool.storage_path
 
 def get_app_config(key: str) -> Any:
     value = APP_CONFIG[key]
@@ -21,7 +21,12 @@ def get_app_config(key: str) -> Any:
 
 def setup_app_config(app: Flask):
     CORS(app, origins=get_app_config("ALLOWED_ORIGINS"),  supports_credentials=True)
-    # CORS(app, resources={r"/*": {"origins": "*"}},  supports_credentials=True)
+
+    #stop automate sorting dict response
+    app.json.sort_keys = False
     app.register_blueprint(auth_route)
     app.register_blueprint(tool_route)
     app.register_blueprint(user_route)
+
+def get_local_storage_path():
+    return storage_path
